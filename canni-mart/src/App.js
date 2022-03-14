79fac06729c9bf14be8535caca8e117c86c0f921
Natalie <theliteshado@icloud.com>
2022-03-14 18:02:08 -0400

    Home page

--- canni-mart/src/App.js
+++ canni-mart/src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar.js'
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Route, Redirect } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Home from "./pages/Home.js";
 
 function App() {
   return (
     <div className="App">
       <div className="navbar-container">
-      <Navbar />
-        <p>
-          Hello from App
-        </p>
+        <Navbar />
+        <p>Hello from App</p>
       </div>
+
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return <Home />;
+        }}
+      />
     </div>
   );
 }
---
+++ canni-mart/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = (props) => {
+    return (
+        <div className="home-outer">
+            <div className="home-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
