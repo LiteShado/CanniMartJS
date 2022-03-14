3adc4bbb0bac26bd4990e012c79bb75740822f93
Natalie <theliteshado@icloud.com>
2022-03-14 18:19:02 -0400

    product list component

--- canni-mart/src/App.js
+++ canni-mart/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { Route, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Home from "./pages/Home.js";
+import AllProducts from "./pages/AllProducts.js";
 
 function App() {
   return (
@@ -18,6 +19,14 @@ function App() {
         exact
         render={() => {
           return <Home />;
+        }}
+      />
+
+      <Route
+        path="/allproducts"
+        exact
+        render={() => {
+          return <AllProducts />;
         }}
       />
     </div>
---
+++ canni-mart/src/pages/AllProducts.js
@@ -0,0 +1,10 @@
+import ProductList from '../components/ProductList';
+import AllProductCSS from '../css/AllProductCSS.css'
+
+const AllProduct = (props) => {
+    return (
+        <ProductList />
+    )
+}
+
+export default AllProduct
